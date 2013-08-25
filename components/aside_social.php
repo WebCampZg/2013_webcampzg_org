813bb912eb927f8d67dd659a2bf30cb67ade22c5
Miro Svrtan <miro@mirosvrtan.me>
2013-08-25 19:40:01 +0200

    Added fb & tw links to aside social

--- components/aside_social.php
+++ components/aside_social.php
@@ -1,6 +1,6 @@
 
 <aside class="social">
-    <a class="fb-icon" href="#" target="#"><span>Facebook</span></a>
-    <a class="tw-icon" href="#" target="#"><span>Twitter</span></a>
+    <a class="fb-icon" href="https://www.facebook.com/WebCampZg" target="_blank"><span>Facebook</span></a>
+    <a class="tw-icon" href="https://twitter.com/webcampzg" target="_blank"><span>Twitter</span></a>
     <a class="gp-icon" href="#" target="#"><span>Google +</span></a>
 </aside>
