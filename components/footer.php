a504a3004e85d86402a78b4df6e5b81fcd8f9c00
Miro Svrtan <miro@mirosvrtan.me>
2013-08-25 19:38:09 +0200

    Added GA code to footer

--- components/footer.php
+++ components/footer.php
@@ -2,3 +2,16 @@
 		<footer id="page_footer">
 			<p>Copyright 2013. Webcamp Zagreb. Design & development: <a href="http://www.trikoder.net">Trikoder</a>; Hosted by: <a href="http://dobarkod.hr" target="_blank">Dobar Kod</a></p>
 		</footer>
+
+    <!-- Google analytics -->
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-35198834-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
