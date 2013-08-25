d456db78afc0f892f5decf80f4e301876a59199b
Miro Svrtan <miro@mirosvrtan.me>
2013-08-25 19:25:21 +0200

    Added components

---
+++ components/footer.php
@@ -0,0 +1,4 @@
+
+		<footer id="page_footer">
+			<p>Copyright 2013. Webcamp Zagreb. Design & development: <a href="http://www.trikoder.net">Trikoder</a>; Hosted by: <a href="http://dobarkod.hr" target="_blank">Dobar Kod</a></p>
+		</footer>
---
+++ components/social.php
@@ -0,0 +1,11 @@
+		<div class="social_block">
+			<div class="inner_wrap">
+
+				<h4>Prati nas na društvenim mrežama</h4>
+
+				<a class="fb-icon" href="https://www.facebook.com/WebCampZg" target="_blank">fb.com/webcampzg</a>
+				<a class="tw-icon" href="https://twitter.com/webcampzg" target="_blank">@webcampzg</a>
+				<a class="gp-icon" href="" target="_blank">g+ link</a>
+
+			</div>
+		</div>
