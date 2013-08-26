6c7e20f1773467e9f60906ac1cc6b580b67ba23c
Miro Svrtan <miro@mirosvrtan.me>
2013-08-26 22:23:39 +0200

    g+ removed

--- components/aside_social.php
+++ components/aside_social.php
@@ -2,5 +2,7 @@
 <aside class="social">
     <a class="fb-icon" href="https://www.facebook.com/WebCampZg" target="_blank"><span>Facebook</span></a>
     <a class="tw-icon" href="https://twitter.com/webcampzg" target="_blank"><span>Twitter</span></a>
+<?php if (0) { ?>
     <a class="gp-icon" href="#" target="#"><span>Google +</span></a>
+<?php }?>
 </aside>
