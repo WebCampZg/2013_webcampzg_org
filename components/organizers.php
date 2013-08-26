96e9cc76b2be7f2b2b6d856da19a7ba64b814609
Orioly <ivan@orioly.com>
2013-08-26 23:35:54 +0200

    Added organizers

--- components/organizers.php
+++ components/organizers.php
@@ -1,18 +1,11 @@
-<?php if (1) {?>
 		<div class="sponsors_block">
 			<div class="inner_wrap">
-				<div class="main">
-					<a href="#" target="_blank"><img src="../images/sponsors/net_gen.png" alt="" /></a>
-					<a href="#" target="_blank"><img src="../images/sponsors/dobar_kod.png" alt="" /></a>
-				</div>
-
-				<a href="#" target="_blank"><img src="../images/sponsors/trikoder.png" alt="" /></a>
-				<a href="#" target="_blank"><img src="../images/sponsors/log_it.png" alt="" /></a>
-				<a href="#" target="_blank"><img src="../images/sponsors/adobe.png" alt="" /></a>
-				<a href="#" target="_blank"><img src="../images/sponsors/hgk.png" alt="" /></a>
-				<a href="#" target="_blank"><img src="../images/sponsors/microsoft.png" alt="" /></a>
-				<a href="#" target="_blank"><img src="../images/sponsors/active_collab.png" alt="" /></a>
+				<a href="http://codeatsix.infinum.hr/meetup" target="_blank" title="Code at Six"><img src="../images/organizers/code_at_six.png" alt="Code at Six" /></a>
+            <a href="http://croatia.girlgeekdinners.com/" title="Girl Geek Dinners" target="_blank"><img src="../images/organizers/girl_geek_dinners.png" alt="Girl Geek Dinners" /></a>
+            <a href="http://www.frontman-hr.org/" title="Frontman HR" target="_blank"><img src="../images/organizers/frontman_hr.png" alt="Frontman HR" /></a>
+            <a href="http://www.mscommunity.hr/" title="FutureDEV" target="_blank"><img src="../images/organizers/ms_community.png" alt="FutureDEV" /></a>
+            <a href="http://hujak.hr/" title="HUJAK" target="_blank"><img src="../images/organizers/hujak.png" alt="HUJAK" /></a>
+				<a href="https://groups.google.com/forum/?fromgroups=#!forum/python-hr" title="Python Hrvatska" target="_blank"><img src="../images/organizers/python_hr.png" alt="Python Hrvatska" /></a>
+				<a href="http://zgphp.org/" title="ZgPHP" target="_blank"><img src="../images/organizers/zgphp.png" alt="ZgPHP" /></a>
 			</div>
 		</div>
-
-<?php } ?>
